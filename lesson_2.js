8def6cabd5c3224890f9fc68b10e0ea61cb10c87
unknown <paliakova1987@mail.ru>
2022-04-15 21:32:32 +0300

    case

---
+++ lesson_2.js
@@ -0,0 +1,17 @@
+
+::::::::::::::::::::: case ::::::::::::::::::::::::::::::::
+
+let test_day = 'Monday1'
+
+switch (test_day){
+    case 'Saturday':
+        console.log('Cool! Saturday')
+        break;
+    case 'Monday':
+            console.log('Cool! Monday')
+            break;
+    case 'Saturday':
+            console.log('Cool! Saturday')
+            break;
+    default:
+        console.log('default!!!')
